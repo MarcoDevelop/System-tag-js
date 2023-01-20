ec9b396d3af1f526fd19dd0a960bb866f899e78f
MarcoDevelop <m.miranda2015@outlook.com>
2023-01-19 23:14:49 -0300

    Function query selector

--- js/inputDate.js
+++ js/inputDate.js
@@ -1,5 +1,40 @@
-// let product = prompt("Informe o Nome do Produto:")
-// let rollo = prompt("Informe o N do Rollo:")
+const productClick = document.getElementById('productClick')
+
+productClick.addEventListener('click', (evt) => {
+  let product = prompt("Informe o Nome do Produto:")
+  document.querySelector('#product').textContent = product;
+});
+
+// _____________________________________________________________
+
+const millClick = document.getElementById('millClick')
+
+millClick.addEventListener('click', (evt) => {
+  let mill = prompt("Informe o N° do Mill Rollo:")
+  document.querySelector('#mill').textContent = mill.toUpperCase();
+});
+
+//______________________________________________________________
+
+const rolloClick = document.getElementById('rolloClick')
+
+rolloClick.addEventListener('click', (evt) => {
+  let rollo = prompt("Informe o N° do Mill Rollo:")
+  document.querySelector('#rollo').textContent = rollo.toUpperCase();
+});
+
+//______________________________________________________________
+
+const codProdClick = document.getElementById('codProdClick')
+
+codProdClick.addEventListener('click', (evt) => {
+  let codProd = prompt("Informe o N° do Mill Rollo:")
+  document.querySelector('#codProd').textContent = codProd.toUpperCase();
+});
+
+//______________________________________________________________
+
+
 // let acronymProduct = prompt("Informe a Sigla do Produto, exemplo: (M ST-17)")
 // let millRoll = prompt("Informe o Mill Roll:")
 // let longitud = prompt("Informe a Longitud (Metragem):")
@@ -16,6 +51,6 @@
 
 // let width = prompt("Informe o mm:")
 
-inch = (940.0 / 25.4)
+// inch = (940.0 / 25.4)
 
-alert("O tamanho em inch(Polegadas) é: " + Math.round(inch))
+// alert("O tamanho em inch(Polegadas) é: " + Math.round(inch))
