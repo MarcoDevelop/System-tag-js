bfdc96f582b8541dade0d765f54f3ea08378b863
MarcoDevelop <m.miranda2015@outlook.com>
2023-01-19 22:04:59 -0300

    Projeto em desenvolvimento

---
+++ js/inputDate.js
@@ -0,0 +1,21 @@
+// let product = prompt("Informe o Nome do Produto:")
+// let rollo = prompt("Informe o N do Rollo:")
+// let acronymProduct = prompt("Informe a Sigla do Produto, exemplo: (M ST-17)")
+// let millRoll = prompt("Informe o Mill Roll:")
+// let longitud = prompt("Informe a Longitud (Metragem):")
+
+// ft = (longitud * 3.281)
+
+// alert("A Metragem em Pés é: " + ft)
+
+// let weight = prompt("Informe o Peso (Kg):")
+
+// lb = (weight * 2.205)
+
+// alert("O Peso em Libras é: " + lb)
+
+// let width = prompt("Informe o mm:")
+
+inch = (940.0 / 25.4)
+
+alert("O tamanho em inch(Polegadas) é: " + Math.round(inch))
